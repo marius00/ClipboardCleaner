c16e9d055b240459d63afab2c1247df7767d213e
Marius <e@mail.com>
2019-06-29 13:59:58 -0300

    .

---
+++ ClipboardCleaner/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClipboardCleaner {
+    public partial class Form1 : Form {
+        private readonly LowLevelKeyboardHook _hook;
+        private MinimizeToTray _minimizeToTray;
+
+        public Form1(LowLevelKeyboardHook hook) {
+            InitializeComponent();
+            _hook = hook;
+        }
+
+        private void Form1_Load(object s, EventArgs e) {
+            _hook.OnKeyDown += HookOnOnKeyDown;
+            this.Closing += (sender, args) => _hook.OnKeyDown -= HookOnOnKeyDown;
+            _minimizeToTray = new MinimizeToTray(trayIcon, this);
+            Hide();
+        }
+
+        private void HookOnOnKeyDown(object o, Keys keys) {
+            var isV = (int) keys == 'V';
+            bool ctrlDown = // TODO: Also triggers on shift
+                (LowLevelKeyboardHook.GetKeyState((int) Keys.LControlKey) & 0xF0) != 0 ||
+                (LowLevelKeyboardHook.GetKeyState((int) Keys.RControlKey) & 0xF0) != 0;
+
+
+            if (isV && ctrlDown) {
+                if (Clipboard.ContainsText(TextDataFormat.Html)) {
+                    Clipboard.SetText(Clipboard.GetText(TextDataFormat.UnicodeText));
+                }
+            }
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e) {
+            e.Cancel = false;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e) {
+            Close();
+        }
+       
+    }
+}
---
+++ ClipboardCleaner/MinimizeToTray.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using log4net;
+
+namespace ClipboardCleaner {
+    class MinimizeToTray {
+        private static readonly ILog Logger = LogManager.GetLogger(typeof(Form1));
+        private readonly NotifyIcon _trayIcon;
+        private readonly Form _form;
+
+        public MinimizeToTray(NotifyIcon trayIcon, Form form) {
+            this._trayIcon = trayIcon;
+            this._form = form;
+
+            form.SizeChanged += OnMinimizeWindow;
+        }
+
+        private void OnMinimizeWindow(object sender, EventArgs e) {
+            try {
+                if (_form.WindowState == FormWindowState.Minimized) {
+                    _form.Hide();
+                    _trayIcon.Visible = true;
+                }
+                else {
+                    _trayIcon.Visible = false;
+                }
+            }
+            catch (Exception ex) {
+                Logger.Warn(ex.Message);
+                Logger.Warn(ex.StackTrace);
+            }
+        }
+    }
+}
